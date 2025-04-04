2dae82214bd58c0d8ec9146efd302f511afe774b
Jonathan Maple <jmaple@ciq.com>
2025-04-04 15:25:37 -0400

    LongTerm Rebase
    Add local git branching to programatically create branching strategy and
    rebase.

--- lt_rebase.sh
+++ lt_rebase.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 set -x
 
+UPSTREAM_REF=$1
+if [ -z "$UPSTREAM_REF" ]; then
+    UPSTREAM_REF="stable_6.12.y"
+    echo "UPSTREAM_REF not set, defaulting to $UPSTREAM_REF"
+fi
+
+git fetch --all
+RES=$(git branch --all | grep ciq-6.12.y-next | wc -l)
+if [ $RES -ne 0 ]; then 
+    echo "cit-6.12.y-next branch already exists, please check status of remote and local branches"
+    exit 1
+fi
+
+git checkout -b ciq-6.12.y-next $UPSTREAM_REF
+git checkout ciq-6.12.y
+git checkout -b {automation_tmp}_ciq-6.12.y-next
+git rebase ciq-6.12.y-next
+
+REPO_STATUS=$(git status -s)
+if [ ! -z "$REPO_STATUS" ]; then
+    echo "Repository is not clean, please check status of remote and local branches"
+    exit 1
+fi
+
+
 CIQ_CONFIG_PATH=ciq/configs
 
 CONFIG_LIST=$(ls ${CIQ_CONFIG_PATH}/*.config)
@@ -36,3 +61,12 @@ for config in $CONFIG_LIST; do
 	exit 1
     fi
 done
+
+REPO_STATUS=$(git status -s)
+if [ ! -z "$REPO_STATUS" ]; then
+    echo "Repository is not clean adding configs"
+    git add .
+    git commit -m "[CIQ] $(git describe --tags --abbrev=0) - rebased configs"
+fi
+
+
