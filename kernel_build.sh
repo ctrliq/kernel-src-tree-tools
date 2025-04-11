1af004444a5581befa9a98b7ca5e3fcf7a6c67c6
Brett Mastbergen <bmastbergen@ciq.com>
2025-04-11 10:25:29 -0400

    kernel_build: Prefer unversioned .config
    The unversioned config will be the config syned with the corresponding
    dist-git config.  After that, prefer the -rhel config, and then the
    versioned config.
    
    LE-2785

--- kernel_build.sh
+++ kernel_build.sh
@@ -26,14 +26,12 @@ ARCH=$(uname -m)
 if [ "x86_64" == "${ARCH}" ] || [ "aarch64" == "${ARCH}" ]; then
     VERSION=$(uname -r | cut -d '-' -f1)
     echo "x86_64 architecture detected, copying config"
-    if [ -f configs/kernel-${VERSION}-${ARCH}.config ]; then
-	cp -v configs/kernel-${VERSION}-${ARCH}.config .config
-    elif [ -f configs/kernel-${ARCH}-rhel.config ]; then
-	# Rocky 9 SIG CLOUD
-	cp -v configs/kernel-${ARCH}-rhel.config .config
-    elif [ -f configs/kernel-${ARCH}.config ]; then
-	# Rocky 8 SIG CLOUD
+    if [ -f configs/kernel-${ARCH}.config ]; then
 	cp -v configs/kernel-${ARCH}.config .config
+    elif [ -f configs/kernel-${ARCH}-rhel.config ]; then
+	cp -v configs/kernel-${ARCH}-rhel.config .config
+    elif [ -f configs/kernel-${VERSION}-${ARCH}.config ]; then
+	cp -v configs/kernel-${VERSION}-${ARCH}.config .config
     elif [ -f configs/kernel-rt-${VERSION}-${ARCH}.config ]; then
         cp -v configs/kernel-rt-${VERSION}-${ARCH}.config .config
 	# Some sort of RT build?
