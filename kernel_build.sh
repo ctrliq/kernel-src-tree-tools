ae20c9eacfc35e65c9e945f70db42ff1efca5e8b
Brett Mastbergen <bmastbergen@ciq.com>
2025-04-15 11:43:54 -0400

    kernel_build: Look for configs in ciq/config first

--- kernel_build.sh
+++ kernel_build.sh
@@ -30,7 +30,9 @@ ARCH=$(uname -m)
 if [ "x86_64" == "${ARCH}" ] || [ "aarch64" == "${ARCH}" ]; then
     VERSION=$(uname -r | cut -d '-' -f1)
     echo "${ARCH} architecture detected, copying config"
-    if [ -f configs/kernel-${ARCH}.config ]; then
+    if [ -f ciq/configs/kernel-${ARCH}.config ]; then
+	cp -v ciq/configs/kernel-${ARCH}.config .config
+    elif [ -f configs/kernel-${ARCH}.config ]; then
 	cp -v configs/kernel-${ARCH}.config .config
     elif [ -f configs/kernel-${ARCH}-rhel.config ]; then
 	cp -v configs/kernel-${ARCH}-rhel.config .config
