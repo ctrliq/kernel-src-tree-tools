63e1b6c0a5517febd9bcfcac1fb163c7f0b6da5a
Roxana Nicolescu <rnicolescu@ciq.com>
2025-10-20 15:40:57 +0200

    kt/ktlib: Introduce config helper lib
    Add dataclass Config that would contain the basic configuration for each
    kernel developer. It mainly consists of directories path that will be
    used do the working setup later.
    
    The configuration is read from a json file. The json file name is stored
    in an environment variable "KTOOLS_CONFIG_FILE". If the variable is not
    set, or the file does not exists or the format is not correct, it will
    use a default configuration.
    
    Signed-off-by: Roxana Nicolescu <rnicolescu@ciq.com>

--- kt/KT.md
+++ kt/KT.md
@@ -9,13 +9,40 @@ To keep things clear, it is introduced as a separate module in
 kernel-src-tree-tools and it does not interfere with the current tooling.
 By keeping this under the same repo, it will be easier to refactor things.
 
-Steps:
+## Setup:
+
 1. Install dependencies globally (you can also create a venv) :
 ```
 $ python -m pip install -e ".[dev]"
 ```
-2. The command above will install pre-commit. To run the pre-commit tool
+2. The command above will install pre-commit. To setup the pre-commit tool
 before you commit something, run this:
 ```
 $ pre-commit install
+```
+
+## Implementation details:
+
+kt/ktlib is the place for common helpers that would be used for kt commands.
+
+kt/ktlib.config.py is where a Config dataclass is implemented. This is crucial
+for future commands and for doing the setup of the kernel developer. At the
+moment it contains absolute paths to local directories:
+- The working dir (the root directory for the setup)
+- The directory parent for each kernel directory
+- The parent directory where default images are downloaded
+- The parent directory where running vm images for each kernel are stored.
+
+Each developer has to  provide their own configuration in json file and keep
+the path in KTOOLS_CONFIG_FILE. Otherwise a default one will be used.
+
+Example content of the config file:
+```
+{
+    "base_path": "~/ciq",
+    "kernels_dir": "~/ciq/kernels",
+    "images_source_dir": "~/ciq/default_test_images",
+    "images_dir": "~/ciq/tmp/virt-images",
+    "ssh_key": "~/.ssh/id_ed25519_generic.pub",
+}
 ```
---
+++ kt/ktlib/config.py
@@ -0,0 +1,96 @@
+import json
+import os
+import warnings
+from dataclasses import dataclass
+from typing import ClassVar, Optional
+
+from pathlib3x import Path
+
+CONFIG_FILE_ENV_VAR = "KTOOLS_CONFIG_FILE"
+
+
+@dataclass
+class Config:
+    """
+    Config dataclass that contains the basic paths for each kernel
+    developer setup.
+
+    base_path   The working directory for developers
+    kernels_dir The directory where each kernel working dir will be created
+    images_source_dir The directory where the default images will be
+                        downloaded
+    images_dir The directory where the vm images for each kernel will be
+                        stored
+    ssh_key: Path to the ssh key (public) shared between host and each vms
+
+    All paths should be absolute, to avoid issues later.
+    """
+
+    base_path: Path
+    kernels_dir: Path
+    images_source_dir: Path
+    images_dir: Path
+
+    ssh_key: Path
+
+    DEFAULT: ClassVar = {
+        "base_path": "~/ciq",
+        "kernels_dir": "~/ciq/kernels",
+        "images_source_dir": "~/ciq/default_test_images",
+        "images_dir": "~/ciq/tmp/virt-images",
+        "ssh_key": "~/.ssh/id_ed25519_generic.pub",
+    }
+
+    @classmethod
+    def from_str_dict(cls, data: dict[str, str]):
+        # Transform the str values to Path
+        new_data = {k: Path(v).expanduser() for k, v in data.items()}
+
+        if not all(v.is_absolute() for v in new_data.values()):
+            raise ValueError("all paths should be absolute; check your config")
+
+        return cls(**new_data)
+
+    @classmethod
+    def load(cls):
+        """Load the default configuration.
+
+        The configuration is loaded in this order from:
+
+        1. The filename provided in KTOOLS_CONFIG_FILE env var;
+            filename type is a json file
+        2. The default configuration
+
+        """
+
+        filename = os.getenv(CONFIG_FILE_ENV_VAR, None)
+        return cls.from_filename(filename)
+
+    @classmethod
+    def from_filename(cls, filename: Optional[str]):
+        """Load config from filename"""
+
+        if filename is None:
+            return cls.from_str_dict(cls.DEFAULT)
+
+        if not os.path.exists(filename):
+            warnings.warn(f"{filename} does not exist, using default config.")
+            return cls.from_str_dict(cls.DEFAULT)
+
+        with open(filename) as jfd:
+            json_data = jfd.read()
+
+        return cls.from_json(json_data)
+
+    @classmethod
+    def from_json(cls, json_data: Optional[str]):
+        if json_data is None:
+            return cls.from_str_dict(cls.DEFAULT)
+
+        try:
+            data = json.loads(json_data)
+        except ValueError:
+            warnings.warn("Invalid configuration, using default config.")
+            data = dict(cls.DEFAULT)
+
+        return cls.from_str_dict(data)
