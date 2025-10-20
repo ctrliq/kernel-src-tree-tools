57b38aec9e21f2a5c0c1d5819898a3f3a0cdf244
Roxana Nicolescu <rnicolescu@ciq.com>
2025-10-20 16:05:54 +0200

    kt: Add checkout command
    Usage: kt checkout <kernel>
    
    This will set up the working directory for a kernel.
    It will create git worktrees for dist-git-tree and kernel-src-tree repos.
    If used if the working directory for a kernel is created,
    the two git worktrees are updated.
    
    An --extra option was added if people need 2 working environments for the
    same kernel at the same time.
    
    Example without --extra:
    
    $ kt checkout lts9-2
    Output:
    $KERNELS_DIR/lts-9.2 will be created with 2 git worktrees:
    kernel-src-tree -- for branch {USER}/ciqlts9_2
    kernel-dist-git -- for branch {USER}/ciqlts9_2
    
    Example with --extra:
    
    $ kt checkout lts9-2 --extra CVE_X
    Output:
    $KERNELS_DIR/lts-9.2_CVE_X will be created with 2 git worktrees:
    kernel-src-tree -- for branch {USER}/ciqlts9_2_CVE_X
    kernel-dist-git -- for branch {USER}/ciqlts9_2_CVE_X
    
    TODO: Autocomplete suggests only the kernel in data/kernels.yaml, not the
    kernel workspace at the moment. This will be improved later, since
    kt checkout may be used to just update en existing kernel workspace.
    
    Signed-off-by: Roxana Nicolescu <rnicolescu@ciq.com>

---
+++ kt/commands/checkout/command.py
@@ -0,0 +1,72 @@
+import click
+
+from kt.commands.checkout.impl import main
+from kt.ktlib.shell_completion import ShellCompletion
+
+epilog = """
+Prepares the working directory for a kernel.
+It uses the KernelInfo dataclass created based on the kernels.yaml file.
+
+The working directory location is based on configuration:
+<config.kernels_dir>/<kernel>/
+
+2 git worktrees are created for a kernel:
+
+- dist-git-tree
+
+- kernel-src-tree
+
+They will point out to their root sources. Check kt setup for more info.
+They should be located in <config.base_path>.
+
+The worktrees reference the remote <branch> from kernels.yaml.
+The local branch is {<user>}/<branch>.
+
+Examples:
+
+\b
+$ kt checkout lts9_4
+\b
+$ kt checkout lts9_4 --cleanup
+\b
+$ kt checkout lts9_4 --cleanup -c
+\b
+$ kt checkout lts9_4 --cleanup --change-dir
+\b
+$ kt checkout lts9_4 -e CVE-2022-49909
+Will create folder lts9_4_CVE-2022-49909 instead of lts9_4.
+"""
+
+
+@click.command(epilog=epilog)
+@click.option(
+    "-c",
+    "--change-dir",
+    is_flag=True,
+    help="Change directory to the kernel directory",
+)
+@click.option(
+    "--override",
+    is_flag=True,
+    help="Delete existing worktree for the kernel and start again",
+)
+@click.option(
+    "--cleanup",
+    is_flag=True,
+    help="Delete existing worktree for the kernel",
+)
+@click.option(
+    "-e",
+    "--extra",
+    type=str,
+    help="Feature you'll be working on",
+)
+@click.argument("kernel", required=True, type=str, shell_complete=ShellCompletion.show_kernels)
+def checkout(kernel, change_dir, override, cleanup, extra):
+    main(
+        name=kernel,
+        change_dir=change_dir,
+        override=override,
+        cleanup=cleanup,
+        extra=extra,
+    )
