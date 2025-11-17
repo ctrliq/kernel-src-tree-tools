38c98019cc35c2265931872031bec5adf4623202
Roxana Nicolescu <rnicolescu@ciq.com>
2025-11-17 13:19:19 +0100

    release_config.py: Ruff formatting
    This was done automatically by ruff format. Default configuration is used.
    
    Signed-off-by: Roxana Nicolescu <rnicolescu@ciq.com>

--- release_config.py
+++ release_config.py
@@ -25,49 +25,29 @@ jira_field_map = {
 
 # Product to branch mapping
 release_map = {
-    "lts-9.4": {
-        "src_git_branch": "ciqlts9_4",
-        "dist_git_branch": "lts94-9",
-        "mock_config": "rocky-lts94"
-    },
-    "lts-9.2": {
-        "src_git_branch": "ciqlts9_2",
-        "dist_git_branch": "lts92-9",
-        "mock_config": "rocky-lts92"
-    },
-    "lts-8.8": {
-        "src_git_branch": "ciqlts8_8",
-        "dist_git_branch": "lts88-8",
-        "mock_config": "rocky-lts88"
-    },
-    "lts-8.6": {
-        "src_git_branch": "ciqlts8_6",
-        "dist_git_branch": "lts86-8",
-        "mock_config": "rocky-lts86"
-    },
-    "cbr-7.9": {
-        "src_git_branch": "ciqcbr7_9",
-        "dist_git_branch": "cbr79-7",
-        "mock_config": "centos-cbr79"
-    },
+    "lts-9.4": {"src_git_branch": "ciqlts9_4", "dist_git_branch": "lts94-9", "mock_config": "rocky-lts94"},
+    "lts-9.2": {"src_git_branch": "ciqlts9_2", "dist_git_branch": "lts92-9", "mock_config": "rocky-lts92"},
+    "lts-8.8": {"src_git_branch": "ciqlts8_8", "dist_git_branch": "lts88-8", "mock_config": "rocky-lts88"},
+    "lts-8.6": {"src_git_branch": "ciqlts8_6", "dist_git_branch": "lts86-8", "mock_config": "rocky-lts86"},
+    "cbr-7.9": {"src_git_branch": "ciqcbr7_9", "dist_git_branch": "cbr79-7", "mock_config": "centos-cbr79"},
     "fips-9.2": {
         "src_git_branch": "fips-9-compliant/5.14.0-284.30.1",
         "dist_git_branch": "el92-fips-compliant-9",
-        "mock_config": "rocky-fips92"
+        "mock_config": "rocky-fips92",
     },
     "fips-8.10": {
         "src_git_branch": "fips-8-compliant/4.18.0-553.16.1",
         "dist_git_branch": "el810-fips-compliant-8",
-        "mock_config": "rocky-fips810-553-depot"
+        "mock_config": "rocky-fips810-553-depot",
     },
     "fips-8.6": {
         "src_git_branch": "fips-8-compliant/4.18.0-553.16.1",
         "dist_git_branch": "el86-fips-compliant-8",
-        "mock_config": "rocky-fips86-553-depot"
+        "mock_config": "rocky-fips86-553-depot",
     },
     "fipslegacy-8.6": {
         "src_git_branch": "fips-legacy-8-compliant/4.18.0-425.13.1",
         "dist_git_branch": "fips-compliant8",
-        "mock_config": "rocky-lts86-fips"
-    }
+        "mock_config": "rocky-lts86-fips",
+    },
 }
