6bb9129a8b0c739ac98e9fbb288562d837421a4b
Roxana Nicolescu <rnicolescu@ciq.com>
2025-11-24 17:13:27 +0100

    ciq-cherry-pick.py: Improve error handling
    All exceptions are propapagated to main and handled there, except for
    CherryPickException that is raised only if cherry pick failed due to conflicts.
    This way user is not overwhelmed with a stacktrace when the error is obvious.
    For unexpected errors, the full stacktrace is shown.
    
    Also added some comments to full_cherry_pick function to explain that
    if one of the cherry pick fails, the previous successful ones are left intact.
    This can be improved in the future by making it interactive.
    
    Signed-off-by: Roxana Nicolescu <rnicolescu@ciq.com>

--- ciq-cherry-pick.py
+++ ciq-cherry-pick.py
@@ -4,6 +4,7 @@ import os
 import re
 import subprocess
 import sys
+import traceback
 
 import git
 
@@ -14,11 +15,16 @@ from ciq_helpers import (
     CIQ_fixes_references,
     CIQ_get_full_hash,
     CIQ_original_commit_author_to_tag_string,
+    CIQ_reset_HEAD,
     CIQ_run_git,
 )
 
 MERGE_MSG = git.Repo(os.getcwd()).git_dir + "/MERGE_MSG"
 MERGE_MSG_BAK = f"{MERGE_MSG}.bak"
+
+
+class CherryPickException(Exception):
+    pass
 
 
 def check_fixes(sha):
@@ -58,8 +64,11 @@ def manage_commit_message(full_sha, ciq_tags, jira_ticket, commit_successful):
         raise RuntimeError(f"Could not find author of commit {full_sha}")
 
     new_tags.append(author)
-    with open(MERGE_MSG, "r") as file:
-        original_msg = file.readlines()
+    try:
+        with open(MERGE_MSG, "r") as file:
+            original_msg = file.readlines()
+    except IOError as e:
+        raise RuntimeError(f"Failed to read commit message from {MERGE_MSG}: {e}") from e
 
     optional_msg = "" if commit_successful else "upstream-diff |"
     new_msg = CIQ_cherry_pick_commit_standardization(
@@ -69,8 +78,11 @@ def manage_commit_message(full_sha, ciq_tags, jira_ticket, commit_successful):
     print(f"Cherry Pick New Message for {full_sha}")
     print(f"\n Original Message located here: {MERGE_MSG_BAK}")
 
-    with open(MERGE_MSG, "w") as file:
-        file.writelines(new_msg)
+    try:
+        with open(MERGE_MSG, "w") as file:
+            file.writelines(new_msg)
+    except IOError as e:
+        raise RuntimeError(f"Failed to write commit message to {MERGE_MSG}: {e}") from e
 
 
 def cherry_pick(sha, ciq_tags, jira_ticket):
@@ -80,6 +92,8 @@ def cherry_pick(sha, ciq_tags, jira_ticket):
         - MERGE_MSG.bak contains the original commit message
         - MERGE_MSG contains the standardized commit message
         - Conflict has to be solved manually
+    In case of runtime errors that are not cherry pick conflicts, the cherry
+    pick changes are reverted. (git reset --hard HEAD)
 
     In case of success:
         - the commit is cherry picked
@@ -88,7 +102,10 @@ def cherry_pick(sha, ciq_tags, jira_ticket):
     """
 
     # Expand the provided SHA1 to the full SHA1 in case it's either abbreviated or an expression
-    full_sha = CIQ_get_full_hash(repo=os.getcwd(), short_hash=sha)
+    try:
+        full_sha = CIQ_get_full_hash(repo=os.getcwd(), short_hash=sha)
+    except RuntimeError as e:
+        raise RuntimeError(f"Invalid commit SHA {sha}: {e}") from e
 
     check_fixes(sha=full_sha)
 
@@ -99,16 +116,20 @@ def cherry_pick(sha, ciq_tags, jira_ticket):
     except RuntimeError:
         commit_successful = False
 
-    manage_commit_message(
-        full_sha=full_sha, ciq_tags=ciq_tags, jira_ticket=jira_ticket, commit_successful=commit_successful
-    )
+    try:
+        manage_commit_message(
+            full_sha=full_sha, ciq_tags=ciq_tags, jira_ticket=jira_ticket, commit_successful=commit_successful
+        )
+    except RuntimeError as e:
+        CIQ_reset_HEAD(repo=os.getcwd())
+        raise RuntimeError(f"Could not create proper commit message: {e}") from e
 
     if not commit_successful:
         error_str = (
             f"[FAILED] git cherry-pick -nsx {full_sha}\n"
             "Manually resolve conflict and add explanation under `upstream-diff` tag in commit message\n"
         )
-        raise RuntimeError(error_str)
+        raise CherryPickException(error_str)
 
     CIQ_run_git(repo_path=os.getcwd(), args=["commit", "-F", MERGE_MSG])
 
@@ -132,6 +153,9 @@ def cherry_pick_fixes(sha, ciq_tags, jira_ticket, upstream_ref):
 def full_cherry_pick(sha, ciq_tags, jira_ticket, upstream_ref):
     """
     Cherry picks a commit from upstream-ref along with its Fixes: references.
+    If cherry-pick or cherry_pick_fixes fail, the exception is propagated
+    If one of the cherry picks fails, an exception is returned and the previous
+    successful cherry picks are left as they are.
     """
     # Cherry pick the commit
     cherry_pick(sha=sha, ciq_tags=ciq_tags, jira_ticket=jira_ticket)
@@ -180,6 +204,10 @@ if __name__ == "__main__":
 
     try:
         full_cherry_pick(sha=args.sha, ciq_tags=tags, jira_ticket=args.ticket, upstream_ref=args.upstream_ref)
-    except Exception as e:
+    except CherryPickException as e:
         logging.error(e)
+        sys.exit(1)
+    except Exception as e:
+        logging.error(f"full_cherry_pick failed {e}")
+        traceback.print_exc()
         sys.exit(1)
