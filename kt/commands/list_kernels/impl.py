1f11c845d97f5b511c62adb3c2c8ea360471c0a4
Roxana Nicolescu <rnicolescu@ciq.com>
2026-03-05 11:28:20 +0100

    kt/list-kernels: Add --automated option
    It will show only the kernels that are automated in kernelCI.
    
    Signed-off-by: Roxana Nicolescu <rnicolescu@ciq.com>

--- kt/commands/list_kernels/impl.py
+++ kt/commands/list_kernels/impl.py
@@ -2,8 +2,10 @@ from kt.ktlib.config import Config
 from kt.ktlib.kernels import KernelsInfo
 
 
-def main():
+def main(automated: bool = False):
     config = Config.load()
     kernels = KernelsInfo.from_yaml(config=config).kernels
-    for k in sorted(kernels):
-        print(k)
+
+    for k in sorted(kernels.values(), key=lambda k: k.name):
+        if not automated or k.automated:
+            print(k.name)
