cfce9e02670fa32004147eda098f8f5b25402d6c
Roxana Nicolescu <rnicolescu@ciq.com>
2026-03-05 11:28:20 +0100

    kt/list-kernels: Add --automated option
    It will show only the kernels that are automated in kernelCI.
    
    Signed-off-by: Roxana Nicolescu <rnicolescu@ciq.com>

--- kt/commands/list_kernels/command.py
+++ kt/commands/list_kernels/command.py
@@ -4,15 +4,31 @@ from kt.commands.list_kernels.impl import main
 
 epilog = """
 It list all the kernels we currently maintain.
+If --automated or -a is used, it will list all the kernels we maintain
+but are also part of KernelCI.
 
 Example:
 
 \b
 $ kt list-kernels
+cbr-7.9
+fipslegacy-8.6
+lts-8.6
+lts-9.2
+lts-9.4
+lts-9.6
+
+\b
+$ kt list-kernels --automated
+lts-8.6
+lts-9.2
+lts-9.4
+lts-9.6
 
 """
 
 
 @click.command(epilog=epilog)
-def list_kernels():
-    main()
+@click.option("-a", "--automated", is_flag=True, help="It selects only automated kernels")
+def list_kernels(automated):
+    main(automated)
