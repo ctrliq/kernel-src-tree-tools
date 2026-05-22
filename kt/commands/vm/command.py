21b6e9197cff3a650d87a88ae3a9249ae0b0b8cc
Roxana Nicolescu <rnicolescu@ciq.com>
2026-05-22 12:32:56 +0200

    kt vm: Introduce --override-base
    This will destroy the vm first if it already exists, but it will also
    delete the base image from <config.images_source_dir> and download it again,
    instead of reusing the same base.
    
    Signed-off-by: Roxana Nicolescu <rnicolescu@ciq.com>

--- kt/commands/vm/command.py
+++ kt/commands/vm/command.py
@@ -17,6 +17,12 @@ they will have their own configuration and image.
 cloud-init.yaml configuration is taken from kt/data and modified accordingly
 for each user and then put in the same folder.
 
+If the base image exists at <config.images_source_dir>, it will be reused
+forever, unless parameter --override-base is used to download the
+base image and replace it. Note, this won't interfere with existing
+vms that share the same base because their own image is copied in their
+own <config.images_dir>/<kernel> folder.
+
 Examples:
 
 \b
@@ -29,7 +35,8 @@ $ kt vm lts9_4 -c
 $ kt vm lts9_4 --destroy
 \b
 $ kt vm lts9_4 -c --override
-
+\b
+$ kt vm lts9_4 -c --override-base
 """
 
 
@@ -52,6 +59,11 @@ $ kt vm lts9_4 -c --override
     help="It destroys the vm if it exists and creates a new one",
 )
 @click.option(
+    "--override-base",
+    is_flag=True,
+    help="It destroys the vm if it exists and creates a new one, but it will also override the base qcow image",
+)
+@click.option(
     "--list-all",
     is_flag=True,
     help="Lists existings vms",
@@ -60,7 +72,7 @@ $ kt vm lts9_4 -c --override
 @click.option("--vcpus", type=int, default=12, help="Number of virtual CPUs (default: 12)")
 @click.option("--memory", type=int, default=32768, help="Memory in MiB (default: 32768)")
 @click.argument("kernel_workspace", required=False, shell_complete=ShellCompletion.show_kernel_workspaces)
-def vm(kernel_workspace, console, destroy, override, list_all, test, vcpus, memory):
+def vm(kernel_workspace, console, destroy, override, override_base, list_all, test, vcpus, memory):
     if not list_all and not kernel_workspace:
         raise click.UsageError("kernel_workspace is required unless --list-all is specified")
 
@@ -69,6 +81,7 @@ def vm(kernel_workspace, console, destroy, override, list_all, test, vcpus, memo
         console=console,
         destroy=destroy,
         override=override,
+        override_base=override_base,
         list_all=list_all,
         test=test,
         vcpus=vcpus,
